b7673aa8760d71a9aa41d45533686613ac540dc8
Eric Horodyski <horodyski@ionic.io>
2020-05-04 15:20:31 -0400

    feat(auth0): add Auth0 provider

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,5 @@
+import { Auth0Options } from './Auth0Options';
 import { AzureOptions } from './AzureOptions';
 import { CognitoOptions } from './CognitoOptions';
 
-export { AzureOptions, CognitoOptions };
+export { Auth0Options, AzureOptions, CognitoOptions };
